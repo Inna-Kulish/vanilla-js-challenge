58207ac368e801be1d7ecf837f35f8348173839f
Inna-Kulish <norelajest@gmail.com>
2024-01-26 13:54:47 +0200

    add click event on buttons

--- js/01-drum-kit.js
+++ js/01-drum-kit.js
@@ -1,27 +1,31 @@
-const keys = document.querySelector('.keys');
-keys.addEventListener('transitionend', removeTransition);
+const keys = Array.from(document.querySelectorAll('.key'));
+keys.forEach(key => {
+  key.addEventListener('transitionend', removeTransition);
+  key.addEventListener('click', playOnClick)
+});
+  window.addEventListener('keydown', playOnKeydown);
+
+function playOnClick(e) {
+  const itemKey = e.currentTarget;
+  const audio = document.querySelector(`audio[data-key="${itemKey.dataset.key}"]`);
+
+  itemKey.classList.add('playing');
+  audio.currentTime = 0;
+  audio.play();
+  }
 
 function removeTransition(e) {
-  if (!e.target.classList.contains('key')) return;
-    
-  if (e.propertyName !== 'transform') return;
-
+ if (e.propertyName !== 'transform') return;
     e.target.classList.remove('playing');
   }
 
-window.addEventListener('keydown', playSound);
-
-function playSound(e) {
+function playOnKeydown(e) {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-    const key = document.querySelector(`li[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`li[data-key="${e.keyCode}"]`);
     if (!audio) return;
 
     key.classList.add('playing');
     audio.currentTime = 0;
     audio.play();
-  }
-
-  // Another way to get list
-  // const keys = Array.from(document.querySelectorAll('.key'));
-  // keys.forEach(key => key.addEventListener('transitionend', removeTransition));
-  
+}
+ 
